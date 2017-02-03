1f5102b3821c8c02379a524efcf8f5fbac9e14c7
Joshua Koudys <josh@qaribou.com>
2017-02-03 13:00:08 -0500

    Remove last of json helper

--- web/concrete/tools/pages/autocomplete.php
+++ web/concrete/tools/pages/autocomplete.php
@@ -1,22 +1,21 @@
 <?php
-defined('C5_EXECUTE') or die("Access Denied.");
 $valt = Loader::helper('validation/token');
 if ($valt->validate('quick_page_select_' . $_REQUEST['key'], $_REQUEST['token'])) {
-	$u = new User();
-	Loader::model('page_list');
-	$db = Loader::db();
-	$pl = new PageList();
-	if ($_GET['term'] != '') {
-		$pl->filterByName($_GET['term']);
-	}
-	$pages = $pl->getPage();
-	$pageNames = array();
-	foreach($pages as $c) {
-		$obj = new stdClass;
-		$obj->label = $c->getCollectionName();
-		$obj->value = $c->getCollectionID();
-		$pageNames[] = $obj;
-	}
-	$jh = Loader::helper('json');
-	echo $jh->encode($pageNames);
+    $u = new User();
+    Loader::model('page_list');
+    $db = Loader::db();
+    $pl = new PageList();
+    if ($_GET['term'] != '') {
+        $pl->filterByName($_GET['term']);
+    }
+
+    echo json_encode(array_map(
+        function ($c) {
+            return [
+                'label' => $c->getCollectionName(),
+                'value' => $c->getCollectionID(),
+            ];
+        },
+        (array) $pl->getPage()
+    ));
 }
