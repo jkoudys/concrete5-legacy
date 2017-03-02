25f84443d407d6ad691f60001668b557f92197e4
Joshua Koudys <josh@qaribou.com>
2017-03-02 12:19:48 -0500

    Auto-style to eslint standard

--- web/js/ccm_app/dashboard.js
+++ web/js/ccm_app/dashboard.js
@@ -1,111 +1,110 @@
-ccm_closeDashboardPane = function(r) {
-	$(r).closest('div.ccm-pane').fadeOut(120, 'easeOutExpo');
-}
+ccm_closeDashboardPane = function (r) {
+	                    $(r).closest('div.ccm-pane').fadeOut(120, 'easeOutExpo');
+};
 
-ccm_getDashboardBackgroundImageData = function(image, display) {
-	$.getJSON(CCM_TOOLS_PATH + '/dashboard/get_image_data', {
-		'image': image
-	}, function(r) {
-		if (r && display) {
-			var html = '<div>';
-			html += '<strong>' + r.title + '</strong> ' + ccmi18n.authoredBy + ' ';
-			if (r.link) {
-				html += '<a target="_blank" href="' + r.link + '">' + r.author + '</a>';
+ccm_getDashboardBackgroundImageData = function (image, display) {
+	                    $.getJSON(CCM_TOOLS_PATH + '/dashboard/get_image_data', {
+		                    'image': image,
+	}, (r) => {
+		                    if (r && display) {
+			                  let html = '<div>';
+			                    html += '<strong>' + r.title + '</strong> ' + ccmi18n.authoredBy + ' ';
+			                    if (r.link) {
+				                    html += '<a target="_blank" href="' + r.link + '">' + r.author + '</a>';
 			} else {
-				html += r.author;
+				                    html += r.author;
 			}
-			$('<div id="ccm-dashboard-image-caption" class="ccm-ui"/>').html(html).appendTo(document.body).show();
-			setTimeout(function() {
-				$('#ccm-dashboard-image-caption').fadeOut(1000, 'easeOutExpo');
+			                    $('<div id="ccm-dashboard-image-caption" class="ccm-ui"/>').html(html).appendTo(document.body).show();
+			                    setTimeout(() => {
+				                    $('#ccm-dashboard-image-caption').fadeOut(1000, 'easeOutExpo');
 			}, 5000);
 		}
 	});
-}
+};
 
-var lastSizeCheck = 9999999;		
-ccm_testFixForms = function() {
-	if ($(window).width() <= 560 && lastSizeCheck > 560) {
-		ccm_fixForms();
+let lastSizeCheck = 9999999;
+ccm_testFixForms = function () {
+	                    if ($(window).width() <= 560 && lastSizeCheck > 560) {
+		                    ccm_fixForms();
 	} else if ($(window).width() > 560 && lastSizeCheck <= 560) {
-		ccm_fixForms(true);
+		                    ccm_fixForms(true);
 	}
-	lastSizeCheck = $(window).width();
-}
-ccm_fixForms = function(horizontal) {
-	$('form').each(function() {
-		var f = $(this);
-		if (horizontal) {
-			if (f.attr('original-class') == 'form-horizontal') {
-				f.attr('class', '').addClass('form-horizontal');
+	                    lastSizeCheck = $(window).width();
+};
+ccm_fixForms = function (horizontal) {
+	                    $('form').each(function () {
+		                const f = $(this);
+		                    if (horizontal) {
+			                    if (f.attr('original-class') == 'form-horizontal') {
+				                    f.attr('class', '').addClass('form-horizontal');
 			}
 		} else {
-			f.removeClass('form-horizontal');
+			                    f.removeClass('form-horizontal');
 		}
 	});
-}
+};
 
-ccm_dashboardEqualizeMenus = function() {
-	if ($(window).width() < 560) {
-		$('div.dashboard-icon-list div.well').css('visibility', 'visible');
-		return false;
+ccm_dashboardEqualizeMenus = function () {
+	                    if ($(window).width() < 560) {
+		                    $('div.dashboard-icon-list div.well').css('visibility', 'visible');
+		                    return false;
 	}
-	var j = -1;
-	var i;
-	var pos = 0;
-	var menus = new Array();
-	$('ul.nav-list').each(function() {		
-		if ($(this).position().top != pos) {
-			j++;
-			menus[j] = new Array();
+	                  let j = -1;
+	                  let i;
+	                  let pos = 0;
+	                const menus = new Array();
+	                    $('ul.nav-list').each(function () {
+		                    if ($(this).position().top != pos) {
+			                    j++;
+			                    menus[j] = new Array();
 		}
-		
-		menus[j].push($(this));
-		pos = $(this).position().top;
+
+		                    menus[j].push($(this));
+		                    pos = $(this).position().top;
 	});
-	
-	for (i = 0; i < menus.length; i++) {
-		var h = 0;
-		for (j = 0; j < menus[i].length; j++) {
-			var mx = menus[i][j];
-			if (mx.height() > h) {
-				h = mx.height();
+
+	                    for (i = 0; i < menus.length; i++) {
+		                  let h = 0;
+		                    for (j = 0; j < menus[i].length; j++) {
+			                    var mx = menus[i][j];
+			                    if (mx.height() > h) {
+				                    h = mx.height();
 			}
-		}	
-		for (j = 0; j < menus[i].length; j++) {
-			var mx = menus[i][j];
-			mx.css('height', h);
+		}
+		                    for (j = 0; j < menus[i].length; j++) {
+			                    var mx = menus[i][j];
+			                    mx.css('height', h);
 		}
 	}
-	$('div.dashboard-icon-list div.well').css('visibility', 'visible');
-}
+	                    $('div.dashboard-icon-list div.well').css('visibility', 'visible');
+};
 
-$(function() {
-	ccm_activateToolbar();
-	
-	var $ccmPageHelp = $("#ccm-page-help").popover({
-		trigger: 'click',
-		content: function() {
-		var id = $(this).attr('id') + '-content';
-		return $('#' + id).html();
-		
-	}, placement: 'bottom', html: true})
-	.click(function(e) {
-		e.stopPropagation();
+$(() => {
+	                    ccm_activateToolbar();
+
+	                const $ccmPageHelp = $('#ccm-page-help').popover({
+		                    trigger: 'click',
+		                    content() {
+		                const id = $(this).attr('id') + '-content';
+		                    return $('#' + id).html();
+	}, placement: 'bottom', html: true })
+	.click((e) => {
+		                    e.stopPropagation();
 	});
-	if ($ccmPageHelp.length) {
-		$(document).click(function() {
-			$ccmPageHelp.data('popover').hide();
+	                    if ($ccmPageHelp.length) {
+		                    $(document).click(() => {
+			                    $ccmPageHelp.data('popover').hide();
 		});
 	}
-	$('.launch-tooltip').tooltip({placement: 'bottom'});
-	if ($('#ccm-dashboard-result-message').length > 0) { 
-		if ($('.ccm-pane').length > 0) { 
-			var pclass = $('.ccm-pane').parent().attr('class');
-			var gpclass = $('.ccm-pane').parent().parent().attr('class');
-			var html = $('#ccm-dashboard-result-message').html();
-			$('#ccm-dashboard-result-message').html('<div class="' + gpclass + '"><div class="' + pclass + '">' + html + '</div></div>').fadeIn(400);
+	                    $('.launch-tooltip').tooltip({ placement: 'bottom' });
+	                    if ($('#ccm-dashboard-result-message').length > 0) {
+		                    if ($('.ccm-pane').length > 0) {
+			                const pclass = $('.ccm-pane').parent().attr('class');
+			                const gpclass = $('.ccm-pane').parent().parent().attr('class');
+			                const html = $('#ccm-dashboard-result-message').html();
+			                    $('#ccm-dashboard-result-message').html('<div class="' + gpclass + '"><div class="' + pclass + '">' + html + '</div></div>').fadeIn(400);
 		}
 	} else {
-		$("#ccm-dashboard-result-message").fadeIn(200);
+		                    $('#ccm-dashboard-result-message').fadeIn(200);
 	}
 });
--- web/js/ccm_app/status_bar.js
+++ web/js/ccm_app/status_bar.js
@@ -1,181 +1,177 @@
 ccm_statusBar = {
-	
-	items: [],	
 
-	addItem: function(item) {
-		this.items.push(item);
+	                    items: [],
+
+	                    addItem(item) {
+		                    this.items.push(item);
 	},
 
-	activate: function(containerID) {
-		if (!containerID) {
-			containerID = 'ccm-page-controls-wrapper';
+	                    activate(containerID) {
+		                    if (!containerID) {
+			                    containerID = 'ccm-page-controls-wrapper';
 		}
 
-		if (this.items.length > 0) { 
-			var d = '<div id="ccm-page-status-bar" class="ccm-ui">';
-			for (i = 0; i < this.items.length; i++) {
-				var it = this.items[i];
-				var buttonStr = '';
-				var buttons = it.getButtons();
-				for (j = 0; j < buttons.length; j++) {
-					attribs = '';
-					var innerButtonLeft = '';
-					var innerButtonRight = '';
-					if (buttons[j].getInnerButtonLeftHTML() != '') {
-						innerButtonLeft = buttons[j].getInnerButtonLeftHTML() + ' ';
+		                    if (this.items.length > 0) {
+			                  let d = '<div id="ccm-page-status-bar" class="ccm-ui">';
+			                    for (i = 0; i < this.items.length; i++) {
+				                const it = this.items[i];
+				                  let buttonStr = '';
+				                const buttons = it.getButtons();
+				                    for (j = 0; j < buttons.length; j++) {
+					                    attribs = '';
+					                  let innerButtonLeft = '';
+					                  let innerButtonRight = '';
+					                    if (buttons[j].getInnerButtonLeftHTML() != '') {
+						                    innerButtonLeft = buttons[j].getInnerButtonLeftHTML() + ' ';
 					}
-					if (buttons[j].getInnerButtonRightHTML() != '') {
-						innerButtonRight = ' ' + buttons[j].getInnerButtonRightHTML();
+					                    if (buttons[j].getInnerButtonRightHTML() != '') {
+						                    innerButtonRight = ' ' + buttons[j].getInnerButtonRightHTML();
 					}
-					var _attribs = buttons[j].getAttributes();
-					for (k in _attribs) {
-						attribs += _attribs[k].key + '="' + _attribs[k].value + '" ';
+					                const _attribs = buttons[j].getAttributes();
+					                    for (k in _attribs) {
+						                    attribs += _attribs[k].key + '="' + _attribs[k].value + '" ';
 					}
-					if (buttons[j].getURL() != '') {
-						buttonStr += '<a href="' + buttons[j].getURL() + '" ' + attribs + ' class="btn btn-small ' + buttons[j].getCSSClass() + '">' + innerButtonLeft + buttons[j].getLabel() + innerButtonRight + '</a>';
-					} else { 
-						buttonStr += '<button type="submit" ' + attribs + ' name="action_' + buttons[j].getAction() + '" class="btn-small btn ' + buttons[j].getCSSClass() + '">' + innerButtonLeft + buttons[j].getLabel() + innerButtonRight + '</button>';
+					                    if (buttons[j].getURL() != '') {
+						                    buttonStr += '<a href="' + buttons[j].getURL() + '" ' + attribs + ' class="btn btn-small ' + buttons[j].getCSSClass() + '">' + innerButtonLeft + buttons[j].getLabel() + innerButtonRight + '</a>';
+					} else {
+						                    buttonStr += '<button type="submit" ' + attribs + ' name="action_' + buttons[j].getAction() + '" class="btn-small btn ' + buttons[j].getCSSClass() + '">' + innerButtonLeft + buttons[j].getLabel() + innerButtonRight + '</button>';
 					}
 				}
-				var line = '<form method="post" action="' + it.getAction() + '" id="ccm-status-bar-form-' + i + '" ' + (it.useAjaxForm ? 'class="ccm-status-bar-ajax-form"' : '') + '><div class="alert-message alert ' + it.getCSSClass() + '"><button type="button" class="close" data-dismiss="alert">×</button><span>' + it.getDescription() + '</span> <div class="ccm-page-status-bar-buttons">' + buttonStr + '</div></div></form>';
-				d += line;
+				                const line = '<form method="post" action="' + it.getAction() + '" id="ccm-status-bar-form-' + i + '" ' + (it.useAjaxForm ? 'class="ccm-status-bar-ajax-form"' : '') + '><div class="alert-message alert ' + it.getCSSClass() + '"><button type="button" class="close" data-dismiss="alert">×</button><span>' + it.getDescription() + '</span> <div class="ccm-page-status-bar-buttons">' + buttonStr + '</div></div></form>';
+				                    d += line;
 			}
-			d += '</div>';
-			$('#' + containerID).append(d);
-			$('#ccm-page-status-bar .dialog-launch').dialog();
-			$('#ccm-page-status-bar .alert').bind('closed', function() {
-				$(this).remove();
-				var visi = $('#ccm-page-status-bar .alert:visible').length;
-				if (visi == 0) {
-					$('#ccm-page-status-bar').remove();
+			                    d += '</div>';
+			                    $('#' + containerID).append(d);
+			                    $('#ccm-page-status-bar .dialog-launch').dialog();
+			                    $('#ccm-page-status-bar .alert').bind('closed', function () {
+				                    $(this).remove();
+				                const visi = $('#ccm-page-status-bar .alert:visible').length;
+				                    if (visi == 0) {
+					                    $('#ccm-page-status-bar').remove();
 				}
 			});
-			$('#ccm-page-status-bar .ccm-status-bar-ajax-form').ajaxForm({
-				dataType: 'json',
-				beforeSubmit: function() {
-					jQuery.fn.dialog.showLoader();
+			                    $('#ccm-page-status-bar .ccm-status-bar-ajax-form').ajaxForm({
+				                    dataType: 'json',
+				                    beforeSubmit() {
+					                    jQuery.fn.dialog.showLoader();
 				},
-				success: function(r) {
-					if (r.redirect) {
-						window.location.href = r.redirect;
+				                    success(r) {
+					                    if (r.redirect) {
+						                    window.location.href = r.redirect;
 					}
-				}
+				},
 			});
 		}
-	}
+	},
 
-}
+};
 
-ccm_statusBarItem = function() {
+ccm_statusBarItem = function () {
+	                    this.css = '';
+	                    this.description = '';
+	                    this.buttons = [];
+	                    this.action = '';
+	                    this.useAjaxForm = false;
 
-	this.css = '';
-	this.description = '';
-	this.buttons = [];
-	this.action = '';
-	this.useAjaxForm = false;
-	
-	this.setCSSClass = function(css) {
-		this.css = css;
-	}
-	
-	this.enableAjaxForm = function() {
-		this.useAjaxForm = true;
-	}
+	                    this.setCSSClass = function (css) {
+		                    this.css = css;
+	};
 
-	this.setDescription = function(description) {
-		this.description = description;
-	}
-	
-	this.getCSSClass = function() {
-		return this.css;
-	}
-	
-	this.getDescription = function() {
-		return this.description;
-	}
-	
-	this.addButton = function(btn) {
-		this.buttons.push(btn);
-	}
-	
-	this.getButtons = function() {
-		return this.buttons;
-	}
-	
-	this.setAction = function(action) {
-		this.action = action;
-	}
-	
-	this.getAction = function() {
-		return this.action;
-	}
+	                    this.enableAjaxForm = function () {
+		                    this.useAjaxForm = true;
+	};
 
-}
+	                    this.setDescription = function (description) {
+		                    this.description = description;
+	};
 
-ccm_statusBarItemButton = function() {
-	
-	this.css = '';
-	this.innerbuttonleft = '';
-	this.innerbuttonright = '';
-	this.label = '';
-	this.action = '';
-	this.url = '';
-	this.attribs = new Array();
-	
-	this.setLabel = function(label) {
-		this.label = label;
-	}
-	
-	this.setCSSClass = function(css) {
-		this.css = css;
-	}
+	                    this.getCSSClass = function () {
+		                    return this.css;
+	};
 
-	this.setInnerButtonLeftHTML = function(html) {
-		this.innerbuttonleft = html;
-	}
+	                    this.getDescription = function () {
+		                    return this.description;
+	};
 
-	this.setInnerButtonRightHTML = function(html) {
-		this.innerbuttonright = html;
-	}
-	
-	this.setAction = function(action) {
-		this.action = action;
-	}
-	
-	this.getAttributes = function() {
-		return this.attribs;
-	}
-	
-	this.addAttribute = function(key, value) {
-		this.attribs.push({'key': key, 'value': value});
-	}
-	
-	this.getAction = function() {
-		return this.action;
-	}
+	                    this.addButton = function (btn) {
+		                    this.buttons.push(btn);
+	};
 
-	this.setURL = function(url) {
-		this.url = url;
-	}
-	
-	this.getURL = function() {
-		return this.url;
-	}
-	
-	this.getCSSClass = function() {
-		return this.css;
-	}
-	
-	this.getInnerButtonLeftHTML = function() {
-		return this.innerbuttonleft;
-	}
+	                    this.getButtons = function () {
+		                    return this.buttons;
+	};
 
-	this.getInnerButtonRightHTML = function() {
-		return this.innerbuttonright;
-	}
-	
-	this.getLabel = function() {
-		return this.label;
-	}
+	                    this.setAction = function (action) {
+		                    this.action = action;
+	};
 
-}
+	                    this.getAction = function () {
+		                    return this.action;
+	};
+};
+
+ccm_statusBarItemButton = function () {
+	                    this.css = '';
+	                    this.innerbuttonleft = '';
+	                    this.innerbuttonright = '';
+	                    this.label = '';
+	                    this.action = '';
+	                    this.url = '';
+	                    this.attribs = new Array();
+
+	                    this.setLabel = function (label) {
+		                    this.label = label;
+	};
+
+	                    this.setCSSClass = function (css) {
+		                    this.css = css;
+	};
+
+	                    this.setInnerButtonLeftHTML = function (html) {
+		                    this.innerbuttonleft = html;
+	};
+
+	                    this.setInnerButtonRightHTML = function (html) {
+		                    this.innerbuttonright = html;
+	};
+
+	                    this.setAction = function (action) {
+		                    this.action = action;
+	};
+
+	                    this.getAttributes = function () {
+		                    return this.attribs;
+	};
+
+	                    this.addAttribute = function (key, value) {
+		                    this.attribs.push({ 'key': key, 'value': value });
+	};
+
+	                    this.getAction = function () {
+		                    return this.action;
+	};
+
+	                    this.setURL = function (url) {
+		                    this.url = url;
+	};
+
+	                    this.getURL = function () {
+		                    return this.url;
+	};
+
+	                    this.getCSSClass = function () {
+		                    return this.css;
+	};
+
+	                    this.getInnerButtonLeftHTML = function () {
+		                    return this.innerbuttonleft;
+	};
+
+	                    this.getInnerButtonRightHTML = function () {
+		                    return this.innerbuttonright;
+	};
+
+	                    this.getLabel = function () {
+		                    return this.label;
+	};
+};
