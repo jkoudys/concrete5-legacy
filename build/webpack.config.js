27bd24692d83c28b97ee15eb63daa0b4c80d0e7b
Joshua Koudys <josh@qaribou.com>
2017-03-03 11:39:55 -0500

    Split build targets for base (small) vs app

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -4,14 +4,25 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // Check if we're doing a production build
 const PROD = process.env.NODE_ENV === 'production';
+const target = process.env.TARGET;
 
 const presets = ['babel-preset-es2015', 'babel-preset-es2017', 'babel-preset-stage-2'].map(require.resolve);
 
 const appDir = '../web/js/ccm_app';
 
-module.exports = Object.assign({
-  entry: [
-    'babel-polyfill',
+const entry = [];
+const output = {
+  path: '../web/js',
+};
+
+
+if (target === 'base') {
+  entry.push(
+    'babel-polyfill'
+  );
+  output.filename = 'ccm.base.js';
+} else {
+  entry.push(
     `${appDir}/jquery.colorpicker.js`,
     `${appDir}/jquery.hoverIntent.js`,
     `${appDir}/jquery.liveupdate.js`,
@@ -33,12 +44,15 @@ module.exports = Object.assign({
     `${appDir}/ui.js`,
     `${appDir}/toolbar.js`,
     `${appDir}/themes.js`,
-    `${appDir}/composer.js`,
-  ],
-  output: {
-    path: '../web/js',
-    filename: 'ccm.app.js',
-  },
+    `${appDir}/composer.js`
+  );
+
+  output.filename = 'ccm.app.js';
+}
+
+module.exports = Object.assign({
+  entry,
+  output,
   module: {
     loaders: [{
       test: /\.json$/,
